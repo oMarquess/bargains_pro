e4e3a4710059f15261f8ad294c95994868374ade
oMarquess <pymasterclass@gmail.com>
2023-11-05 13:10:19 +0000

    product card added

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -17,7 +17,23 @@ const ProductCard = ({product}:Props) => {
         alt={product.title}
         height = {200}
         width={200}
-        className='product-card_img'/>
+        className='product-card_img'
+        />
+    </div>
+    <div className="flex flex-col gap-3">
+        <h3 className='product-title'>{product.title}</h3>
+
+        <div className="flex justify-between">
+            <p className='text-black opacity-50 text-lg capitalize'>
+                {product.category}
+            </p>
+            <p className="text-black text-lg font-semibold">
+                <span>{product?.currency}</span>
+                <span>{product?.currentPrice}</span>
+            </p>
+
+        </div>
+
     </div>
     </Link>
   )
